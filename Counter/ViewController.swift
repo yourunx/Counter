9b6ed36dea93dfe5ac3bc2211f3ecfba267685aa
Practicumyourunx <workserm777@gmail.com>
2024-10-21 14:12:26 +0300

    Первый коммит

---
+++ Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by qwerty on 21.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var counter:Int: = 0
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
